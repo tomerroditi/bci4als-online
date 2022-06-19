6c3986a5bd7f34890612ae9c89771cd1450683cc
tomerroditi <89650204+tomerroditi@users.noreply.github.com>
2022-06-19 17:03:34 +0300

    scripts

--- 1.scripts/to_do_list.m
+++ 1.scripts/to_do_list.m
@@ -3,11 +3,10 @@
 % - create a VAE with the EEGNet structure (need to create custom layers
 % for the decoder... this will take alot of work)
 % - finish the script for preprocessing hyperparameters optimization
+% - figure out how to implement tall arrays for continuous segmentations
+%   for when we have too much data to handle at once..
 
 % small tasks:
-% - add a function to validate the options structure which alert if
-% unsupported values are givven and asigns default values for missing
-% fields. insert this function in 'recording' constructor!
-% - reject corapted trials using voltage threshold (in continuous
-% segmentation)
+% - fix the fc activation visualization
+% - verify the location of the electrodes in the plot weights function (very important!)
 
